108c67f85fffb8ef0a072305b3e2d683fc6af9c0
Artur Prepelytsia <preart81@gmail.com>
2025-01-20 02:51:35 +0200

    final
    add api, services
    add main.py
    fix upcoming_birthdays func

--- src/schemas/contacts.py
+++ src/schemas/contacts.py
@@ -7,15 +7,6 @@ from pydantic import BaseModel, ConfigDict, EmailStr, Field, field_validator
 class ContactBase(BaseModel):
     """
     ContactBase schema for contact information.
-    Attributes:
-        first_name (str): The first name of the contact. Must be between 2 and 50 characters.
-        last_name (str): The last name of the contact. Must be between 2 and 50 characters.
-        email (EmailStr): The email address of the contact.
-        phone_number (str): The phone number of the contact. Must be between 6 and 20 characters.
-        birthday (date): The birthday of the contact. Cannot be a future date.
-        additional_data (Optional[str]): Additional information about the contact. Maximum length is 150 characters.
-    Methods:
-        validate_birthday(cls, v): Validates that the birthday is not in the future.
     """
 
     first_name: str = Field(max_length=50, min_length=2)
@@ -26,19 +17,7 @@ class ContactBase(BaseModel):
     additional_data: Optional[str] = Field(max_length=150)
 
     @field_validator("birthday")
-    def validate_birthday(self, v):
-        """
-        Validates that the provided birthday is not in the future.
-
-        Args:
-            v (date): The birthday to validate.
-
-        Raises:
-            ValueError: If the birthday is in the future.
-
-        Returns:
-            date: The validated birthday.
-        """
+    def validate_birthday(cls, v):
         if v > date.today():
             raise ValueError("Birthday cannot be in the future")
         return v
@@ -48,12 +27,6 @@ class ContactResponse(ContactBase):
     """
     ContactResponse is a data model that extends ContactBase and includes additional fields for
     id, created_at, and updated_at.
-    Attributes:
-        id (int): The unique identifier for the contact.
-        created_at (datetime | None): The timestamp when the contact was created. Can be None.
-        updated_at (Optional[datetime] | None): The timestamp when the contact was last updated. Can be None.
-    Config:
-        model_config (ConfigDict): Configuration dictionary with attributes settings.
     """
 
     id: int
